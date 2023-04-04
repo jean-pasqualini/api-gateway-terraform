fc5979e5c790772ecb9470be6f57336576b99b2b
Jean Pasqualini <jpasqualini75@gmail.com>
2023-04-04 13:52:43 +0200

    initial state

---
+++ api_gateway_terraform_mode.tf
@@ -0,0 +1,54 @@
+resource "aws_api_gateway_rest_api" "rest_api_terraform_mode" {
+  name = "rest-api-terraform-mode"
+}
+
+resource "aws_api_gateway_resource" "rest_api_terraform_mode_first_level" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api_terraform_mode.id
+  parent_id   = aws_api_gateway_rest_api.rest_api_terraform_mode.root_resource_id
+  path_part   = "first_level"
+}
+
+resource "aws_api_gateway_resource" "rest_api_terraform_mode_second_level" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api_terraform_mode.id
+  parent_id   = aws_api_gateway_resource.rest_api_terraform_mode_first_level.id
+  path_part   = "second_level"
+}
+
+resource "aws_api_gateway_method" "rest_api_terraform_mode_get_method" {
+  rest_api_id   = aws_api_gateway_rest_api.rest_api_terraform_mode.id
+  resource_id   = aws_api_gateway_resource.rest_api_terraform_mode_second_level.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "rest_api_terraform_mode_get_integration" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api_terraform_mode.id
+  resource_id = aws_api_gateway_resource.rest_api_terraform_mode_second_level.id
+  http_method = aws_api_gateway_method.rest_api_terraform_mode_get_method.http_method
+  type        = "MOCK"
+  request_templates = {
+    "application/json" = <<EOF
+    $input.json('$')
+    EOF
+  }
+}
+
+resource "aws_api_gateway_deployment" "rest_api_terraform_mode_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api_terraform_mode.id
+  stage_name = "current"
+
+  depends_on = [
+    aws_api_gateway_integration.rest_api_terraform_mode_get_integration
+  ]
+
+  description       = "Deployed at ${timestamp()}"
+  stage_description = "Deployed at ${timestamp()}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  variables = {
+    deployed_at = "Deployed at ${timestamp()}"
+  }
+}
